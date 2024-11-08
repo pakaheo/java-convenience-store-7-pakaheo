22e145b09b15f3e5fd91d78ea21d1b52d3fe2a65
pakaheo <k70923@konkuk.ac.kr>
2024-11-09 01:53:10 +0900

    feat : Create product

--- src/main/java/store/Product.java
+++ src/main/java/store/Product.java
@@ -1,2 +1,35 @@
-package store;public class Product {
+package store;
+
+import java.util.Objects;
+
+public class Product {
+
+    private final String name;
+    private final int price;
+    private int quantity;
+    private final String promotionName;
+
+    public Product(final String name, final int price, int quantity, final String promotionName) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotionName = promotionName;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Product product)) {
+            return false;
+        }
+        return price == product.price && quantity == product.quantity && Objects.equals(name, product.name)
+                && Objects.equals(promotionName, product.promotionName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, quantity, promotionName);
+    }
 }
---
+++ src/test/java/store/ProductTest.java
@@ -0,0 +1,17 @@
+package store;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+
+    @Test
+    void 상품_생성() {
+        Product coke = new Product("콜라", 1_000, 10, "탄산2+1");
+        Product cider = new Product("사이다", 1_000, 10, null);
+
+        assertThat(coke).isEqualTo(new Product("콜라", 1_000, 10, "탄산2+1"));
+        assertThat(cider).isEqualTo(new Product("사이다", 1_000, 10, null));
+    }
+}
