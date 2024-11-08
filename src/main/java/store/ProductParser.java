fe840c3f9f9c56029fb5d90186a3eb81e79543a3
pakaheo <k70923@konkuk.ac.kr>
2024-11-09 01:54:31 +0900

    feat : ProductParser 생성
    ProductParser는 문자열로 된 상품 정보들을 상품 리스트로 바꾼다

--- src/main/java/store/ProductParser.java
+++ src/main/java/store/ProductParser.java
@@ -1,2 +1,28 @@
-package store;public class ProductParser {
+package store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductParser {
+
+    private static final String SEPARATE = ",";
+
+    public static List<Product> parse(List<String> productContents) {
+        List<Product> productGroup = new ArrayList<>();
+
+        for (String content : productContents) {
+            String[] splitted = splitBySeparate(content);
+            productGroup.add(new Product(splitted[0], toInt(splitted[1]), toInt(splitted[2]), splitted[3]));
+        }
+
+        return productGroup;
+    }
+
+    private static String[] splitBySeparate(String content) {
+        return content.split(SEPARATE);
+    }
+
+    private static int toInt(String text) {
+        return Integer.parseInt(text);
+    }
 }
--- src/test/java/store/ProductParserTest.java
+++ src/test/java/store/ProductParserTest.java
@@ -1,2 +1,17 @@
-package store;public class ProductParserTest {
+package store;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ProductParserTest {
+
+    @Test
+    void 상품_리스트_생성() {
+        List<String> productContents = List.of("콜라,1000,10,탄산2+1", "사이다,1000,10,null");
+
+        assertThat(ProductParser.parse(productContents)).hasSize(2);
+        assertThat(ProductParser.parse(productContents).getFirst()).isInstanceOf(Product.class);
+    }
 }
