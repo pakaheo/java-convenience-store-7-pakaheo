77eb09c48cbb76fc87553b60efd7ddf68fdfcc07
pakaheo <k70923@konkuk.ac.kr>
2024-11-09 14:07:47 +0900

    refactor : parse 메소드 분리

--- src/main/java/store/OrderDetailsParser.java
+++ src/main/java/store/OrderDetailsParser.java
@@ -8,15 +8,37 @@ import java.util.regex.Pattern;
 public class OrderDetailsParser {
 
     private static final Pattern PATTERN = Pattern.compile("\\[([가-힣]+)-(\\d+)]");
+    private static final int PRODUCT_NAME_PLACE = 1;
+    private static final int PURCHASE_COUNT_PLACE = 2;
 
     public static Map<String, Integer> parse(String input) {
         Map<String, Integer> orders = new HashMap<>();
+        Matcher matcher = createMatcher(input);
 
-        Matcher matcher = PATTERN.matcher(input);
-        while (matcher.find()) {
-            orders.put(matcher.group(1), Integer.parseInt(matcher.group(2)));
+        while (canFind(matcher)) {
+            orders.put(getProductName(matcher), getPurchaseCount(matcher));
         }
 
         return orders;
+    }
+
+    private static Matcher createMatcher(String input) {
+        return PATTERN.matcher(input);
+    }
+
+    private static boolean canFind(Matcher matcher) {
+        return matcher.find();
+    }
+
+    private static String getProductName(Matcher matcher) {
+        return matcher.group(PRODUCT_NAME_PLACE);
+    }
+
+    private static int getPurchaseCount(Matcher matcher) {
+        return toInt(matcher.group(PURCHASE_COUNT_PLACE));
+    }
+
+    private static int toInt(String text) {
+        return Integer.parseInt(text);
     }
 }
