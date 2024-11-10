d5d965bd5a8c585994635e5c2b2cc0ff554353a0
pakaheo <k70923@konkuk.ac.kr>
2024-11-10 13:34:37 +0900

    refactor : 예외 처리 책임 이동
    재고 수량 초과 및 존재하지 않는 상품 입력 시 예외 발생을 Products가 아닌
    OrderDetails가 맡도록 변경

--- src/main/java/store/OrderDetails.java
+++ src/main/java/store/OrderDetails.java
@@ -1,14 +1,45 @@
 package store;
 
+import constants.ErrorMessage;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
+import store.inventory.PromotionalInventory;
+import store.inventory.RegularInventory;
+import store.product.Product;
+import store.product.Products;
 
 public class OrderDetails {
 
     private final Map<String, Integer> orders;
+    private final Products products;
 
-    public OrderDetails(String input) {
+    public OrderDetails(String input, Products products) {
         this.orders = OrderDetailsParser.parse(input);
+        this.products = products;
+        validateInput(orders);
+    }
+
+    private void validateInput(Map<String, Integer> orders) {
+        for (Entry<String, Integer> entry : orders.entrySet()) {
+            checkExistProduct(entry.getKey());
+            checkQuantity(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public void checkExistProduct(String productName) {
+        Product promotionProduct = PromotionalInventory.PROMOTIONAL_INVENTORY.findByName(productName);
+        Product regularProduct = RegularInventory.REGULAR_INVENTORY.findByName(productName);
+
+        if (promotionProduct == null && regularProduct == null) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_EXISTS_PRODUCT.valueOf());
+        }
+    }
+
+    public void checkQuantity(String productName, int purchaseCount) {
+        if (products.isExceedQuantity(productName, purchaseCount)) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEED_QUANTITY.valueOf());
+        }
     }
 
     public Map<String, Integer> getOrders() {
--- src/test/java/store/OrderDetailsTest.java
+++ src/test/java/store/OrderDetailsTest.java
@@ -1,27 +1,48 @@
 package store;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import constants.ErrorMessage;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import store.product.Products;
 
 public class OrderDetailsTest {
 
+    private static final Products PRODUCTS = new Products(List.of("콜라,1000,10,null"));
+
     @Test
     void 주문내역_생성() {
-        String input = "[콜라-3],[사이다-4]";
+        String input = "[콜라-3]";
 
-        assertThat(new OrderDetails(input)).isEqualTo(new OrderDetails(input));
+        assertThat(new OrderDetails(input, PRODUCTS)).isEqualTo(new OrderDetails(input, PRODUCTS));
     }
 
     @Test
     void 주문내역_불러오기() {
-        String input = "[콜라-3],[사이다-4]";
+        String input = "[콜라-3]";
         Map<String, Integer> orders = new HashMap<>();
         orders.put("콜라", 3);
-        orders.put("사이다", 4);
 
-        assertThat(new OrderDetails(input).getOrders()).isEqualTo(orders);
+        assertThat(new OrderDetails(input, PRODUCTS).getOrders()).isEqualTo(orders);
+    }
+
+    @Test
+    void 존재하지_않는_상품이면_예외() {
+        Products products = new Products(List.of("콜라,1000,10,null"));
+
+        assertThatThrownBy(() -> new OrderDetails("[사이다-5]", PRODUCTS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NOT_EXISTS_PRODUCT.valueOf());
+    }
+
+    @Test
+    void 구매_희망_수량이_재고_수량보다_많으면_예외() {
+        assertThatThrownBy(() -> new OrderDetails("[콜라-11]", PRODUCTS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.EXCEED_QUANTITY.valueOf());
     }
 }
