6b62effdbc6375771c46d250dfa8be1a3271e11e
pakaheo <k70923@konkuk.ac.kr>
2024-11-10 13:45:27 +0900

    feat : 잘못된 입력 예외 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import constants.ErrorMessage;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,6 +16,8 @@ public class InputView {
     private static final String PRODUCTS_FILE_PATH = "src/main/resources/products.md";
     private static final String PROMOTIONS_FILE_PATH = "src/main/resources/promotions.md";
     private static final String NOT_FOUND_FILE = "파일을 읽어들일 수 없습니다.";
+    private static final String YES = "Y";
+    private static final String NO = "N";
 
     public Products inputProducts() {
         List<String> productContents = new ArrayList<>();
@@ -48,11 +51,30 @@ public class InputView {
         return new Promotions(promotionContents);
     }
 
-    public OrderDetails inputProductAndQuantity() {
-        return new OrderDetails(Console.readLine());
+    public OrderDetails inputProductAndQuantity(Products products) {
+        OrderDetails orderDetails = null;
+        try {
+            orderDetails = new OrderDetails(Console.readLine(), products);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            inputProductAndQuantity(products);
+        }
+        return orderDetails;
     }
 
     protected String inputAnswer() {
-        return Console.readLine();
+        String answer;
+        while (true) {
+            answer = Console.readLine();
+            if (isValidInput(answer)) {
+                break;
+            }
+            System.out.println(ErrorMessage.WRONG_INPUT.valueOf());
+        }
+        return answer;
+    }
+
+    private boolean isValidInput(String answer) {
+        return answer.equals(YES) || answer.equals(NO);
     }
 }
